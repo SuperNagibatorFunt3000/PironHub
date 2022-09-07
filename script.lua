fb000f31515f28dd5ea3dac523984d5b8ae617a8
SuperNagibatorFunt3000 <110674014+SuperNagibatorFunt3000@users.noreply.github.com>
2022-09-07 20:54:01 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -465,6 +465,204 @@ Tab:AddButton({
 	Name = "Players ESP",
 	Callback = function()
         local color = BrickColor.new(252, 255, 253)
+        local transparency = .8
+        
+        local Players = game:GetService("Players")
+        local function _ESP(c)
+          repeat wait() until c.PrimaryPart ~= nil
+          for i,p in pairs(c:GetChildren()) do
+            if p.ClassName == "Part" or p.ClassName == "MeshPart" then
+              if p:FindFirstChild("shit") then p.shit:Destroy() end
+              local a = Instance.new("BoxHandleAdornment",p)
+              a.Name = "shit"
+              a.Size = p.Size
+              a.Color = color
+              a.Transparency = transparency
+              a.AlwaysOnTop = true    
+              a.Visible = true    
+              a.Adornee = p
+              a.ZIndex = true    
+        
+            end
+          end
+        end
+        local function ESP()
+          for i,v in pairs(Players:GetChildren()) do
+            if v ~= game.Players.LocalPlayer then
+              if v.Character then
+                _ESP(v.Character)
+              end
+              v.CharacterAdded:Connect(function(chr)
+                _ESP(chr)
+              end)
+            end
+          end
+          Players.PlayerAdded:Connect(function(player)
+            player.CharacterAdded:Connect(function(chr)
+              _ESP(chr)
+            end)  
+          end)
+        end
+        ESP()
+  	end    
+})
+local Section = Tab:AddSection({
+	Name = "Auto Farm"
+})
+Tab:AddButton({
+	Name = "Auto Arcade",
+	Callback = function()
+        loadstring(game.HttpGet("https://gitlab.com/theholysea/yba-auto-arcade/-/raw/main/main.lua", "https://gitlab.com/theholysea/yba-auto-arcade/-/raw/main/main.lua"))();
+  	end    
+})
+local Tab = Window:MakeTab({
+	Name = "Character",
+	PremiumOnly = false
+})
+local Section = Tab:AddSection({
+	Name = "Base character"
+})
+Tab:AddSlider({
+	Name = "Speed",
+	Min = 0,
+	Max = 500,
+	Default = 16,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	Callback = function(Value)
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+	end    
+})
+Tab:AddSlider({
+	Name = "Jump",
+	Min = 0,
+	Max = 500,
+	Default = 5,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	Callback = function(Value)
+		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+	end    
+})
+local Tab = Window:MakeTab({
+	Name = "Misc",
+	PremiumOnly = false
+})
+local Section = Tab:AddSection({
+	Name = "Server"
+})
+Tab:AddButton({
+	Name = "SERVER CRASHER/LAG",
+	Callback = function()
+      		
+
+while wait(0.6) do 
+    game:GetService("NetworkClient"):SetOutgoingKBPSLimit(math.huge)
+    local function getmaxvalue(val)
+    local mainvalueifonetable = 499999
+    if type(val) ~= "number" then
+    return nil
+    end
+    local calculateperfectval = (mainvalueifonetable/(val+2))
+    return calculateperfectval
+    end
+    local function bomb(tableincrease, tries)
+    local maintable = {}
+    local spammedtable = {}
+    table.insert(spammedtable, {})
+    z = spammedtable[1]
+    for i = 1, tableincrease do
+    local tableins = {}
+    table.insert(z, tableins)
+    z = tableins
+    end
+    local calculatemax = getmaxvalue(tableincrease)
+    local maximum
+    if calculatemax then
+    maximum = calculatemax
+    else
+    maximum = 999999
+    end
+    for i = 1, maximum do
+    table.insert(maintable, spammedtable)
+    end
+    for i = 1, tries do
+    game.RobloxReplicatedStorage.SetPlayerBlockList:FireServer(maintable)
+    end
+    end
+    bomb(250, 2) 
+    end
+  	end    
+})
+Tab:AddButton({
+	Name = "Server Hop",
+	Callback = function()
+        local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")()
+
+        module:Teleport(game.PlaceId)
+  	end    
+})
+Tab:AddButton({
+	Name = "rejoin",
+	Callback = function()
+        local ts = game:GetService("TeleportService")
+
+        local p = game:GetService("Players").LocalPlayer
+        
+         
+        
+        ts:Teleport(game.PlaceId, p)
+  	end    
+})
+OrionLib:Init()
+end
+
+if game.PlaceId == 142823291 then
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Piron Hub (Murder Mystery 2)", HidePremium = false, IntroText = "Piron Hub", SaveConfig = true, ConfigFolder = "OrionTest"})
+local Tab = Window:MakeTab({
+	Name = "Меню",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "Само наводка",
+	Callback = function()
+        loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/WRD%20Aimbot.txt"))()
+  	end    
+})
+local Tab = Window:MakeTab({
+	Name = "Телепорты",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Section = Tab:AddSection({
+	Name = "Телепорт на различные места на карте"
+})
+Tab:AddButton({
+	Name = "Телепортироваться на место выбора карты",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-108.017349, 140.673859, 82.2308884, -0.998025894, -4.83964477e-08, 0.0628038421, -4.64278358e-08, 1, 3.28047456e-08, -0.0628038421, 2.98241396e-08, -0.998025894)
+  	end    
+})
+Tab:AddButton({
+	Name = "Телепортироваться в секретку",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-150.215134, 153.073853, 114.63549, 0.99982053, 3.5582306e-09, 0.0189458765, -3.25580829e-09, 1, -1.59932778e-08, -0.0189458765, 1.59287232e-08, 0.99982053)
+  	end    
+})
+local Tab = Window:MakeTab({
+	Name = "Визуально",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Section = Tab:AddSection({
+	Name = "Подсветка"
+})
+Tab:AddButton({
+	Name = "Подсветка игроков",
+	Callback = function()
+        local color = BrickColor.new(252, 255, 253)
 local transparency = .28
 
 local Players = game:GetService("Players")
@@ -668,204 +866,6 @@ while #names > 0 do
 		end
 	end
 end
-  	end    
-})
-local Section = Tab:AddSection({
-	Name = "Auto Farm"
-})
-Tab:AddButton({
-	Name = "Auto Arcade",
-	Callback = function()
-        loadstring(game.HttpGet("https://gitlab.com/theholysea/yba-auto-arcade/-/raw/main/main.lua", "https://gitlab.com/theholysea/yba-auto-arcade/-/raw/main/main.lua"))();
-  	end    
-})
-local Tab = Window:MakeTab({
-	Name = "Character",
-	PremiumOnly = false
-})
-local Section = Tab:AddSection({
-	Name = "Base character"
-})
-Tab:AddSlider({
-	Name = "Speed",
-	Min = 0,
-	Max = 500,
-	Default = 16,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
-	end    
-})
-Tab:AddSlider({
-	Name = "Jump",
-	Min = 0,
-	Max = 500,
-	Default = 5,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
-	end    
-})
-local Tab = Window:MakeTab({
-	Name = "Misc",
-	PremiumOnly = false
-})
-local Section = Tab:AddSection({
-	Name = "Server"
-})
-Tab:AddButton({
-	Name = "SERVER CRASHER/LAG",
-	Callback = function()
-      		
-
-while wait(0.6) do 
-    game:GetService("NetworkClient"):SetOutgoingKBPSLimit(math.huge)
-    local function getmaxvalue(val)
-    local mainvalueifonetable = 499999
-    if type(val) ~= "number" then
-    return nil
-    end
-    local calculateperfectval = (mainvalueifonetable/(val+2))
-    return calculateperfectval
-    end
-    local function bomb(tableincrease, tries)
-    local maintable = {}
-    local spammedtable = {}
-    table.insert(spammedtable, {})
-    z = spammedtable[1]
-    for i = 1, tableincrease do
-    local tableins = {}
-    table.insert(z, tableins)
-    z = tableins
-    end
-    local calculatemax = getmaxvalue(tableincrease)
-    local maximum
-    if calculatemax then
-    maximum = calculatemax
-    else
-    maximum = 999999
-    end
-    for i = 1, maximum do
-    table.insert(maintable, spammedtable)
-    end
-    for i = 1, tries do
-    game.RobloxReplicatedStorage.SetPlayerBlockList:FireServer(maintable)
-    end
-    end
-    bomb(250, 2) 
-    end
-  	end    
-})
-Tab:AddButton({
-	Name = "Server Hop",
-	Callback = function()
-        local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")()
-
-        module:Teleport(game.PlaceId)
-  	end    
-})
-Tab:AddButton({
-	Name = "rejoin",
-	Callback = function()
-        local ts = game:GetService("TeleportService")
-
-        local p = game:GetService("Players").LocalPlayer
-        
-         
-        
-        ts:Teleport(game.PlaceId, p)
-  	end    
-})
-OrionLib:Init()
-end
-
-if game.PlaceId == 142823291 then
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Piron Hub (Murder Mystery 2)", HidePremium = false, IntroText = "Piron Hub", SaveConfig = true, ConfigFolder = "OrionTest"})
-local Tab = Window:MakeTab({
-	Name = "Меню",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Tab:AddButton({
-	Name = "Само наводка",
-	Callback = function()
-        loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/WRD%20Aimbot.txt"))()
-  	end    
-})
-local Tab = Window:MakeTab({
-	Name = "Телепорты",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-local Section = Tab:AddSection({
-	Name = "Телепорт на различные места на карте"
-})
-Tab:AddButton({
-	Name = "Телепортироваться на место выбора карты",
-	Callback = function()
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-108.017349, 140.673859, 82.2308884, -0.998025894, -4.83964477e-08, 0.0628038421, -4.64278358e-08, 1, 3.28047456e-08, -0.0628038421, 2.98241396e-08, -0.998025894)
-  	end    
-})
-Tab:AddButton({
-	Name = "Телепортироваться в секретку",
-	Callback = function()
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-150.215134, 153.073853, 114.63549, 0.99982053, 3.5582306e-09, 0.0189458765, -3.25580829e-09, 1, -1.59932778e-08, -0.0189458765, 1.59287232e-08, 0.99982053)
-  	end    
-})
-local Tab = Window:MakeTab({
-	Name = "Визуально",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-local Section = Tab:AddSection({
-	Name = "Подсветка"
-})
-Tab:AddButton({
-	Name = "Подсветка игроков",
-	Callback = function()
-        local color = BrickColor.new(252, 255, 253)
-        local transparency = .8
-        
-        local Players = game:GetService("Players")
-        local function _ESP(c)
-          repeat wait() until c.PrimaryPart ~= nil
-          for i,p in pairs(c:GetChildren()) do
-            if p.ClassName == "Part" or p.ClassName == "MeshPart" then
-              if p:FindFirstChild("shit") then p.shit:Destroy() end
-              local a = Instance.new("BoxHandleAdornment",p)
-              a.Name = "shit"
-              a.Size = p.Size
-              a.Color = color
-              a.Transparency = transparency
-              a.AlwaysOnTop = true    
-              a.Visible = true    
-              a.Adornee = p
-              a.ZIndex = true    
-        
-            end
-          end
-        end
-        local function ESP()
-          for i,v in pairs(Players:GetChildren()) do
-            if v ~= game.Players.LocalPlayer then
-              if v.Character then
-                _ESP(v.Character)
-              end
-              v.CharacterAdded:Connect(function(chr)
-                _ESP(chr)
-              end)
-            end
-          end
-          Players.PlayerAdded:Connect(function(player)
-            player.CharacterAdded:Connect(function(chr)
-              _ESP(chr)
-            end)  
-          end)
-        end
-        ESP()
   	end    
 })
 local Tab = Window:MakeTab({
